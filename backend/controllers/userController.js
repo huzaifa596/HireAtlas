6a12dd3e74389f743b6968b0ee915a0ceedde023
huzaifa <huzaifanaseer596@gmail.com>
2026-04-19 08:58:40 +0500

    intiating backend for profile

---
+++ backend/controllers/userController.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const {sql,PoolPromise}=require('../config/db');
+
+const getProfile=async(req,res)=>{
+ try{
+    const pool=await PoolPromise;
+    const result=await pool.request()
+    .input('LoggedInUserID',sql.BigInt,req.user.userID)
+    .execute('sp_GetUserProfile');
+    return res.status(200).json({status:'SUCCESS',profile:result.recordset[0]});
+ }
+ catch(err){
+    console.error('Get profile error:',err);
+    return res.status(500).json({status:'ERROR',message:'Could not fetch profile'});
+ }
+}
