e62cae300c09339ee6f829eff85a99705c8b6f31
huzaifa <huzaifanaseer596@gmail.com>
2026-04-27 01:46:55 +0500

    fixed merging

--- backend/routes/dashboardRoutes.js
+++ backend/routes/dashboardRoutes.js
@@ -1,13 +1,11 @@
 const express    = require('express');
 const router     = express.Router();
 const { getPosts,getSinglePost,insert_into_post} = require('../controllers/dashboardController');
-
 const verifyToken = require('../middleware/authMiddleware');
 
 
 router.get('/posts', verifyToken, getPosts);
 router.get('/posts/:postId', verifyToken, getSinglePost);
-
-
+router.post('/posts/createPost', verifyToken, insert_into_post);
 
 module.exports = router;
