6d8131d83d0789b06ac81b99e66edc8fa4f491fc
huzaifa <huzaifanaseer596@gmail.com>
2026-05-02 02:22:28 +0500

    working on user experience

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express=require('express')
 const router=express.Router();
-const {getProfile,updatePersonalInfo,saveEducation}=require('../controllers/userController')
+const {getProfile,updatePersonalInfo,saveEducation,deleteEducation}=require('../controllers/userController')
 const verifyToken = require('../middleware/authMiddleware');
 
 console.log("getProfile:", getProfile);
 router.get('/', verifyToken, getProfile);
 router.patch('/personal',    verifyToken, updatePersonalInfo);  
-router.post('/education',  verifyToken, saveEducation);        
+router.post('/education',  verifyToken, saveEducation);  
+router.delete('/education/:eduId',   verifyToken, deleteEducation);       
 module.exports = router; 
